dc4bacbbf22e84a009e8c769779a8d14052856c1
Alexander Dellwisch <al_de1@uni-bremen.de>
2020-10-20 14:32:17 +0200

    Modifying schroedingersolver and visualizer

--- visualizer.py
+++ visualizer.py
@@ -1,61 +1,79 @@
-"routines for visualizing results from schroedinger-solver"
+#!/usr/bin/env python3
+"""routines for visualizing results from schroedingersolver"""
 import numpy as np
 import matplotlib.pyplot as plt
+import argparse
 
+_DESCRIPTION = "visualizer for the obtained results from the schroedingersolver"
 
-def data_visualizer(scale=1):
-    """reads and plots data from input
+def main():
+    """visualizer for the obtained results from the schroedingersolver
 
     Args:
-        scale: scales the wavefunctions, default = no scaling
+        directory:
+            directory of results from schroedingersolver
     """
 
-    plt.subplot(1, 2, 1)
+    parser = argparse.ArgumentParser(description=_DESCRIPTION)
+    msg = 'Directory (default: .)'
+    parser.add_argument('-d', '--directory', default='.', help=msg)
+    msg = 'Scale factor'
+    
+    args = parser.parse_args()
 
-    potential = np.loadtxt("potential.dat")
-    xx = potential[:, 0]
-    y1 = potential[:, 1]
+    def data_visualizer(args.directory, scale=1):
+        """reads and plots data from input
 
-    aa = np.loadtxt("wavefunctions.dat")
-    bb = np.loadtxt("energies.dat")
-    cc = np.loadtxt("expvalues.dat")
+        Args:
+            scale: directory, scales the wavefunctions, default = no scaling
+        """
 
-    plotnr = 0
-    for wert in bb:
-        eigenwert = []
-        for xwert in potential:
-            eigenwert.append(wert)
-        plt.plot(xx, eigenwert, color="grey")
+        plt.subplot(1, 2, 1)
 
-        if plotnr % 2:
-            color = "red"
-        else:
-            color = "blue"
-        plt.plot(xx, aa[:, plotnr + 1] * scale + wert, color=color)
-        plt.plot(cc[plotnr, 0], wert, marker="x", color="green", mew=2)
-        plotnr += 1
-    plt.plot(xx, y1, color="black", label="potential curve", scaley=False)
+        potential = np.loadtxt(args.directory, "potential.dat")
+        xx = potential[:, 0]
+        y1 = potential[:, 1]
 
-    plt.xlabel("x [Bohr]")
-    plt.ylabel("Energy [Hartree]")
-    plt.title("Potential, eigenstates, <x>")
+        aa = np.loadtxt(args.directory, "wavefunctions.dat")
+        bb = np.loadtxt(args.directory, "energies.dat")
+        cc = np.loadtxt(args.directory, "expvalues.dat")
 
-    plt.subplot(1, 2, 2)
+        plotnr = 0
+        for wert in bb:
+            eigenwert = []
+            for xwert in potential:
+                eigenwert.append(wert)
+                plt.plot(xx, eigenwert, color="grey")
 
-    plotnr = 0
-    for wert in bb:
-        eigenwert = []
-        for xwert in potential:
-            eigenwert.append(wert)
-        plt.plot(xx, eigenwert, color="grey")
-        plt.plot(cc[plotnr, 1], wert, marker="+", color="violet",
-                 markersize=12, mew=2)
-        plotnr += 1
+            if plotnr % 2:
+                color = "red"
+            else:
+                color = "blue"
+                plt.plot(xx, aa[:, plotnr + 1] * scale + wert, color=color)
+                plt.plot(cc[plotnr, 0], wert, marker="x", color="green", mew=2)
+                plotnr += 1
+                plt.plot(xx, y1, color="black", scaley=False)
 
-    plt.xlabel("[Bohr]")
-    plt.title("sigma x")
-    plt.savefig('curves.pdf', format='pdf')
+        plt.xlabel("x [Bohr]")
+        plt.ylabel("Energy [Hartree]")
+        plt.title("Potential, eigenstates, <x>")
+
+        plt.subplot(1, 2, 2)
+
+        plotnr = 0
+        for wert in bb:
+            eigenwert = []
+            for xwert in potential:
+                eigenwert.append(wert)
+                plt.plot(xx, eigenwert, color="grey")
+                plt.plot(cc[plotnr, 1], wert, marker="+", color="violet",
+                         markersize=12, mew=2)
+                plotnr += 1
+
+        plt.xlabel("[Bohr]")
+        plt.title("sigma x")
+        plt.savefig('curves.pdf', format='pdf')
 
 
 if __name__ == '__main__':
-    data_visualizer()
+    main()
